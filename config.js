a879ee3a6cdcac9e8037a1c0c1a2c1fbed7a5c95
Sharky! <jdeurt@gmail.com>
2018-03-25 23:07:04 -0500

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	TOKEN: "NDI1ODIxNDY1MjA0NzUyMzk0.DZNAqQ.5DHs5uSzY5ZzIw7h3Bc3WjeUUL8",
-	LINK: "https://discordapp.com/api/oauth2/authorize?client_id=425821465204752394&permissions=392256&scope=bot"
-};
